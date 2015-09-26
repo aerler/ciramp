794d329edfd85b10b7605dc9829978a36ef4c15e
Valliappa Lakshmanan <valliappa.lakshmanan@climate.com>
2015-09-26 11:08:22 -0700

    lags_and_times

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -1,19 +1,19 @@
 import numpy as np
- 
+
 en_lat_bottom = -5
 en_lat_top = 5
 en_lon_left = 360-170
 en_lon_right = 360-120
- 
+
 def get_enso_mean(tas):
     """The array of mean temperatures in the El Nino 3.4 region at all time points."""
     return tas.loc[:, en_lat_bottom:en_lat_top, en_lon_left:en_lon_right].mean(dim=('lat','lon'))
 
-def get_calif_mean(tas):
+def get_equatorial_mean(tas, x):
     """The array of mean temperatures in the El Nino 3.4 region at all time points."""
-    return tas.loc[:, 32:36, 360-117:360-113].mean(dim=('lat','lon'))
- 
-def calc_feature(enso):
+    return tas.loc[:, -5:5, x:x+5].mean(dim=('lat','lon'))
+
+def make_feature(enso):
     enso_matrix = enso.values.reshape((-1,12))
     count_matrix = np.ones(enso_matrix.shape)
     enso_monthly_mean = (enso_matrix.cumsum(axis=0) / count_matrix.cumsum(axis=0)).ravel()
@@ -22,12 +22,16 @@ def calc_feature(enso):
     enso_valid = enso.values[valid_range]
     return np.array([enso_valid, enso_monthly_mean_valid])
 
+def make_eq_feature(tas, longitude, lag):
+    enso = get_equatorial_mean(tas, longitude)
+    lagged = np.roll(enso, n_lookahead - lag)
+    return make_feature(enso) 
 
 class FeatureExtractor(object):
- 
+
     def __init__(self):
         pass
- 
+
     def transform(self, temperatures_xray, n_burn_in, n_lookahead, skf_is):
         """Combine two variables: the montly means corresponding to the month of the target and 
         the current mean temperature in the El Nino 3.4 region."""
@@ -35,11 +39,9 @@ class FeatureExtractor(object):
         # the burn-in from the beginning and the prediction look-ahead from
         # the end.
         valid_range = range(n_burn_in, temperatures_xray['time'].shape[0] - n_lookahead)
-        enso = get_enso_mean(temperatures_xray['tas'])
-        cali = get_calif_mean(temperatures_xray['tas'])
-        
-        ensof = calc_feature(enso)
-        calif = calc_feature(cali)
-
-        return np.vstack(ensof, calif).T
-
+        features = []
+        for longitude in xrange(180, 300, 5):
+            for lag in xrange(0, 6):
+                features.append(make_eq_feature(temperatures_xray['tas'], longitude, lag))
+        X = np.vstack(features)
+        return X.T

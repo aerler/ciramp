10bbbd4a64dcfe1bf69283787c5ba2d5de51a57e
Valliappa Lakshmanan <valliappa.lakshmanan@climate.com>
2015-09-26 11:27:39 -0700

    lags_and_times

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -6,7 +6,6 @@ en_lon_left = 360-170
 en_lon_right = 360-120
 
 class FeatureExtractor(object):
-    n_lookahead = -1
     
     def __init__(self):
         pass
@@ -18,7 +17,7 @@ class FeatureExtractor(object):
         # This is the range for which features should be provided. Strip
         # the burn-in from the beginning and the prediction look-ahead from
         # the end.
-        valid_range = range(n_burn_in, temperatures_xray['time'].shape[0] - n_lookahead)
+        self.valid_range = range(n_burn_in, temperatures_xray['time'].shape[0] - n_lookahead)
         features = []
         for longitude in xrange(180, 300, 5):
             for lag in xrange(0, 6):
@@ -39,8 +38,8 @@ class FeatureExtractor(object):
         count_matrix = np.ones(enso_matrix.shape)
         enso_monthly_mean = (enso_matrix.cumsum(axis=0) / count_matrix.cumsum(axis=0)).ravel()
         enso_monthly_mean_rolled = np.roll(enso_monthly_mean, self.n_lookahead - 12)
-        enso_monthly_mean_valid = enso_monthly_mean_rolled[valid_range]
-        enso_valid = enso.values[valid_range]
+        enso_monthly_mean_valid = enso_monthly_mean_rolled[self.valid_range]
+        enso_valid = enso.values[self.valid_range]
         return np.array([enso_valid, enso_monthly_mean_valid])
 
     def make_eq_feature(self, tas, longitude, lag):
